63ea6fb76aab950ea9b7e35c0f17b6d2559d2923
Josiah Glosson <soujournme@gmail.com>
2026-03-11 13:34:37 -0500

    Allow toggling architecture support with features

--- bindings/rust/keystone-sys/build.rs
+++ bindings/rust/keystone-sys/build.rs
@@ -9,7 +9,7 @@ use std::os::unix::fs::symlink;
 use std::os::windows::fs::symlink_dir as symlink;
 
 #[cfg(feature = "build_keystone_cmake")]
-use std::path::Path;
+use std::{env, path::Path};
 
 #[cfg(feature = "build_keystone_cmake")]
 fn build_with_cmake() {
@@ -21,11 +21,37 @@ fn build_with_cmake() {
         symlink(keystone_dir, "keystone").expect("failed to symlink keystone");
     }
 
+    let mut targets_to_build = String::new();
+    if env::var_os("CARGO_FEATURE_ARCH_ARM").is_some() {
+        targets_to_build.push_str("ARM;");
+    }
+    if env::var_os("CARGO_FEATURE_ARCH_ARM64").is_some() {
+        targets_to_build.push_str("AArch64;");
+    }
+    if env::var_os("CARGO_FEATURE_ARCH_MIPS").is_some() {
+        targets_to_build.push_str("Mips;");
+    }
+    if env::var_os("CARGO_FEATURE_ARCH_X86").is_some() {
+        targets_to_build.push_str("X86;");
+    }
+    if env::var_os("CARGO_FEATURE_ARCH_PPC").is_some() {
+        targets_to_build.push_str("PowerPC;");
+    }
+    if env::var_os("CARGO_FEATURE_ARCH_SPARC").is_some() {
+        targets_to_build.push_str("Sparc;");
+    }
+    if env::var_os("CARGO_FEATURE_ARCH_SYSTEMZ").is_some() {
+        targets_to_build.push_str("SystemZ;");
+    }
+    if env::var_os("CARGO_FEATURE_ARCH_HEXAGON").is_some() {
+        targets_to_build.push_str("Hexagon;");
+    }
+
     let dest = cmake::Config::new("keystone")
         .define("CMAKE_INSTALL_LIBDIR", "lib")
         .define("BUILD_LIBS_ONLY", "1")
         .define("BUILD_SHARED_LIBS", "OFF")
-        .define("LLVM_TARGETS_TO_BUILD", "ARM;AArch64;Hexagon;Mips;PowerPC;Sparc;SystemZ;X86")
+        .define("LLVM_TARGETS_TO_BUILD", &targets_to_build)
         // Prevent python from leaving behind `.pyc` files which break `cargo package`
         .env("PYTHONDONTWRITEBYTECODE", "1")
         .build();
